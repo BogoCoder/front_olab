31e0055f3f20d86ad94ebb2ba7d2b6bf5f4626fd
ansesame <ansesame@gmail.com>
2021-10-04 09:24:46 -0500

    Revert "Página reservas moderador"
    This reverts commit 5f9266c6c7681f3f0605fa233baad100095c22d6.

--- src/pages/_routes.jsx
+++ src/pages/_routes.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./home";
-import ReservasModeradorPage from "./reservasModerador";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
+        <Route path="/">
           <HomePage />
         </Route>
-
-        <Route path="/reservasModerador">
-          <ReservasModeradorPage />
-        </Route>
-        
       </Switch>
     </BrowserRouter>
   );
