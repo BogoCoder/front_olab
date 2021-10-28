6021bb661331137ee2c17267a887b14eea10e9da
David_Martinez <davidfel.martinez@urosario.edu.co>
2021-10-28 03:09:09 -0500

    Vista_Politicas_admin

--- src/pages/Politicas.jsx
+++ src/pages/Politicas.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Tempadminaux from "../components/Template_admin_aux";
+import Politicaspage from "../components/Politicascomps/page";
+import { v4 as uuidv4 } from "uuid";
 
 const contenido = () => {
+
+  const testing_users = [
+    {id:uuidv4(), nombre:"Juan Gonzales",cargo:"Profedsor"},
+    {id:uuidv4(),nombre:"David Martinez",cargo:"Estudiante"},
+    {id:uuidv4(),nombre:"Samuel Perez",cargo:"Profedsor"},
+    {id:uuidv4(),nombre:"Pedro Martinez",cargo:"Profedsor"},
+    {id:uuidv4(),nombre:"Sandra Saenz",cargo:"Estudiante"},
+    {id:uuidv4(),nombre:"Miguel Gutierrez",cargo:"Profedsor"},
+  ]
+  const testing_accesibility = {abierta: {reserva:24,prestamo:7,renov:3},
+                                restringida: {reserva:48,prestamo:15,renov:4},
+                                confidencial: {reserva:72,prestamo:30,renov:6}
+                                }
+
     return (
-        <div>AAAAAAAAAAAAAAAAAAAAAAAAAAqui Va la componente Politicas(solo admin)</div>
+        <Politicaspage accesibilityconfigs={testing_accesibility} restrictedusers={testing_users} condidenceusers={testing_users}/>
     )
 }
-const Politicas = ({content}) => {
+const Politicas = ({content,admin,user}) => {
   return (
-    <Tempadminaux content={contenido()} admin="true"/>
+    <Tempadminaux content={contenido()} admin={admin} user={user}/>
   )
 }
-
 export default Politicas;
