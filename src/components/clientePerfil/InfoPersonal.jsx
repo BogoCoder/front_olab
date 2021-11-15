89cdacfa32f12b8797905e4842ca59304a6ef159
camilo <camiloaww@gmail.com>
2021-11-15 18:01:44 -0500

    modificacion botones info personal y nuevo modal para cambio contraseña

--- src/components/clientePerfil/InfoPersonal.jsx
+++ src/components/clientePerfil/InfoPersonal.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from 'react';
 import './InfoPersonal.css';
-import Button from '@mui/material/Button';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import ModalContrasena from './modalContrasena';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -15,7 +15,6 @@ const Item = styled(Paper)(({ theme }) => ({
 function InfoPersonal(){
     let nombre = "Luisa Jaramillo"
     let id = 1064824965
-    let contrasena = 'lj2021'
     let email = 'luisa.jaramillo@urosario.edu.co'
     return (
         <React.Fragment>
@@ -25,11 +24,8 @@ function InfoPersonal(){
                 <Item>
                 <ul>
                     <li>
-                        <h3>Nombre</h3>
+                        <h3>Nombre de Usuario</h3>
                         {nombre}
-                    </li>
-                    <li style={{display: "flex", justifyContent: "flex-end"}}>
-                        <Button variant="contained" >Editar</Button>
                     </li>
                 </ul>
                 </Item>
@@ -41,22 +37,6 @@ function InfoPersonal(){
                         <h3>ID</h3>
                         {id}
                     </li>
-                    <li style={{display: "flex", justifyContent: "flex-end"}}>
-                        <Button variant="contained">Editar</Button>
-                    </li>
-                </ul>
-                </Item>
-                </Grid>
-                <Grid item xs={12}>
-                <Item>
-                <ul>
-                    <li>
-                        <h3>Contraseña</h3>
-                        {'*'.repeat(contrasena.length)}
-                    </li>
-                    <li style={{display: "flex", justifyContent: "flex-end"}}>
-                        <Button variant="contained" >Editar</Button>
-                    </li>
                 </ul>
                 </Item>
                 </Grid>
@@ -67,8 +47,18 @@ function InfoPersonal(){
                         <h3>e-mail</h3>
                         {email}
                     </li>
+                </ul>
+                </Item>
+                </Grid>
+                <Grid item xs={12}>
+                <Item>
+                <ul>
+                    <li>
+                        <h3>Contraseña</h3>
+                        {'*'.repeat(8)}
+                    </li>
                     <li style={{display: "flex", justifyContent: "flex-end"}}>
-                        <Button variant="contained">Editar</Button>
+                        <ModalContrasena/>
                     </li>
                 </ul>
                 </Item>
