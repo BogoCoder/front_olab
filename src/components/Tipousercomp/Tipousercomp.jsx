127535926ef9c22f946372c97f14bb8cdf8f3f64
Samuel <victor.perez@urosario.edu.co>
2021-11-23 17:37:11 -0500

    finalizando login

--- src/components/Tipousercomp/Tipousercomp.jsx
+++ src/components/Tipousercomp/Tipousercomp.jsx
@@ -7,7 +7,7 @@ const TipoUsercomp = ({content,admin,tipousuario,nombre}) => {
         <div className="container">
             <div className="t1"><div>Ingresar como:</div></div>
             <div className="Botones">
-                <button type="button" className="bclient btn"><div>Auxiliar</div></button>
+                <button type="button" className="bclient btn"><div>Cliente</div></button>
                 <button type="button" className="bmod btn"><div>Moderador</div></button>
             </div>
             <div className="Footer"></div>
