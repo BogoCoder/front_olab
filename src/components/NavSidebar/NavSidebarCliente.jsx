b02edb0dad3504e5d474e4996b65e168eeb60d92
unknown <miguelvalzul@gmail.com>
2021-11-25 20:04:26 -0500

    Arreglo del home de cliente; Se cambió el navbar y se puso la barra de búsqueda

--- src/components/NavSidebar/NavSidebarCliente.jsx
+++ src/components/NavSidebar/NavSidebarCliente.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/olab_logo.svg";
 import * as FaIcons from "react-icons/fa";
 import * as IoIcons from "react-icons/io";
 import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const WhiteTextTypography = withStyles({
+	root: {
+		color: "black",
+		textAlign: "right",
+	},
+})(Typography);
 
 const NavbarCliente = () => {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<React.Fragment>
 			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
@@ -26,7 +48,11 @@ const NavbarCliente = () => {
 					<div className='collapse navbar-collapse' id='navbarNav'>
 						<ul className='navbar-nav mx-5'>
 							<li className='nav-item'>
-								<a className='nav-link active' aria-current='page' href='#'>
+								<a
+									className='nav-link active'
+									aria-current='page'
+									href='/Cliente'
+								>
 									Home
 								</a>
 							</li>
@@ -35,26 +61,80 @@ const NavbarCliente = () => {
 									TeslaLAB
 								</a>
 							</li>
-							<li className='nav-item'>
+							{/* <li className='nav-item'>
 								<a className='nav-link' href='#'>
 									Carrito de préstamos
 								</a>
-							</li>
+							</li> */}
 						</ul>
-						<div style={{ color: "#0c3779", marginLeft: "20%" }}>
+						<div style={{ color: "#0c3779", margin: "auto" }}>
 							<h3>Laboratorio Tesla</h3>
 						</div>
 					</div>
 				</div>
-				<IconButton>
+				{/* <IconButton>
 					<FaIcons.FaShoppingCart />
 				</IconButton>
 				<IconButton>
 					<IoIcons.IoMdNotifications />
-				</IconButton>
-				<IconButton>
+				</IconButton> */}
+				{/* <IconButton style={{ marginRight: "1em" }}>
 					<Avatar alt='Miguel Valencia' />
-				</IconButton>
+				</IconButton> */}
+				<div>
+					<Grid
+						container
+						alignItems='center'
+						justifyContent='center'
+						spacing={2}
+					>
+						<Grid item xs container direction='column' spacing={2}>
+							<Grid item xs>
+								<WhiteTextTypography variant='caption'>
+									Miguel Valencia Z.
+								</WhiteTextTypography>
+							</Grid>
+						</Grid>
+						<Grid item>
+							<IconButton
+								aria-label='account of current user'
+								aria-controls='menu-appbar'
+								aria-haspopup='true'
+								onClick={handleMenu}
+								size='small'
+								style={{ marginRight: "1em" }}
+							>
+								<Avatar alt='Miguel Valencia' />
+								{/* <Avatar alt='Camioncitos S.A.S.' src={truck_avatar} /> */}
+							</IconButton>
+						</Grid>
+					</Grid>
+
+					<Menu
+						id='menu-appbar'
+						anchorEl={anchorEl}
+						anchorOrigin={{
+							vertical: "top",
+							horizontal: "right",
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: "top",
+							horizontal: "right",
+						}}
+						open={open}
+						onClose={handleClose}
+					>
+						<Link
+							to='/ClientePerfil'
+							style={{ textDecoration: "none", color: "inherit" }}
+						>
+							<MenuItem onClick={handleClose}>Mi cuenta</MenuItem>
+						</Link>
+
+						<MenuItem onClick={handleClose}>Cerrar sesión</MenuItem>
+					</Menu>
+				</div>
 			</nav>
 		</React.Fragment>
 	);
--- src/components/rutas.js
+++ src/components/rutas.js
@@ -1,4 +1,4 @@
 // Para desarrollo localmente
-const rutaApi = "http://18.213.65.172:3000";
+const rutaApi = "http://18.213.65.172:4000";
 const rutaLogin = "http://localhost:3000";
-export {rutaApi, rutaLogin};
+export { rutaApi, rutaLogin };
